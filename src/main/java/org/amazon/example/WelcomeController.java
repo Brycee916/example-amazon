03025747424fc94d0de87fe383dd8a83cb8fd79c
brycee916 <brycechao27@gmail.com>
2026-07-15 22:51:07 -0700

    created service layer

--- src/main/java/org/amazon/example/WelcomeController.java
+++ src/main/java/org/amazon/example/WelcomeController.java
@@ -1,5 +1,6 @@
 package org.amazon.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+    private final WelcomeService welcomeService;
+
+    @Autowired //inject the WelcomeService dependency into this controller automatically
+    public WelcomeController(WelcomeService welcomeService){
+        this.welcomeService = welcomeService;
+    }
+
     @GetMapping("/welcome")
     public String getWelcomeMessage() {
-        return "Welcome to the Spring Boot REST Api"; //automatically converted to json with @RestController
+        return welcomeService.getWelcomeMessage(); //automatically converted to json with @RestController
     }
     
 
---
+++ src/main/java/org/amazon/example/WelcomeService.java
@@ -0,0 +1,11 @@
+package org.amazon.example;
+
+import org.springframework.stereotype.Service;
+
+@Service //indicates that it contains the business logic of application, a BEAM
+public class WelcomeService {
+
+    public String getWelcomeMessage(){
+        return "Welcome to the Spring Boot Rest API";
+    }
+}
