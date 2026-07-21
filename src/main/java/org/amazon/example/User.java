8751e0ad7d281efc2dba76c658e95b60daf34e4d
brycee916 <brycechao27@gmail.com>
2026-07-20 23:53:55 -0700

    created User Model

---
+++ src/main/java/org/amazon/example/User.java
@@ -0,0 +1,17 @@
+package org.amazon.example;
+
+public class User {
+    private String username;
+    private String password;
+    private String role;
+
+    public User(String username, String password, String role){
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public User(){
+        
+    }
+}
