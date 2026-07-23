2b7af61f8968e3c1461300a175954050a9d8ba65
brycee916 <brycechao27@gmail.com>
2026-07-22 22:19:58 -0700

    register api

--- src/main/java/org/amazon/example/RegistrationController.java
+++ src/main/java/org/amazon/example/RegistrationController.java
@@ -2,6 +2,7 @@ package org.amazon.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RegistrationController {
@@ -9,5 +10,15 @@ public class RegistrationController {
     @Autowired
     private UserService userService;
 
+    //display the registration form
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model){
+
+        model.addAttribute("user", new User());
+        return "register"; //routes it
+    }
+
+    //handle the logic
+    @PostMapping("/register")
     
 }
