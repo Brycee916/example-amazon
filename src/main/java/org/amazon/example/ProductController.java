3ccd08089691bd286f87f711e10feae1b5686faf
brycee916 <brycechao27@gmail.com>
2026-07-30 22:16:30 -0700

    fix api

--- src/main/java/org/amazon/example/ProductController.java
+++ src/main/java/org/amazon/example/ProductController.java
@@ -1,9 +1,16 @@
 package org.amazon.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
@@ -12,12 +19,12 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping
+    @GetMapping("/products")
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
-    @PostMapping
+    @PostMapping("/products/new/{id}")
     public Product addProduct(@RequestBody Product product) {
         return productService.addProduct(product);
     }
